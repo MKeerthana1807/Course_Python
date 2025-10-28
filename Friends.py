4d8e58c73342846d7e4c5ef5add6d00f80ad9b9f
Keerthana <124150021@sastra.ac.in>
2025-10-28 10:59:36 +0530

    Add files via upload

---
+++ Friends.py
@@ -0,0 +1,23 @@
+#Family 
+
+print("My Fathers name is Mariappan")
+print("My Mothers name is Vijaya")
+print("I have a one older brother")
+print("His name is Raja")
+print("He is working in chennai")
+print("My father is a Lorry Driver")
+print("My mother is a Home Maker")
+
+#Friends
+print("My Friends Details")
+print("My best friend is Karthick")
+print("we have been friend since college time")
+print("He is very supportive and helpful")
+print("My other good friend is Madhu")
+print("we have been friend since college time")
+print("she is very funny and cheerful")
+print("My another friend is Hari")
+print("we have been friend since college time")
+print("He is very kind and caring")
+print("Thank you!")
+
