af38c0b7f15e8f68391dffd4428450c0d4f74279
MKeerthana1807 <124150021@sastra.ac.in>
2025-10-28 16:44:38 +0530

    Initial project upload

---
+++ variable/Datatype.py
@@ -0,0 +1,28 @@
+Name = "Keerthna"
+Age = 24
+Height = 135.7
+Weight = 40
+city =" Thoothukudi"
+is_Student = False
+marks = 7.43
+pi = 3.14
+school_name = "Star Matriculation Higher Secondary School"
+print(f"My Name is {Name}")
+print(f"My Age is {Age}")
+print(f"My Height is {Height} cm")
+print(f"My Weight is {Weight} kg")
+print(f"My City is {city}")
+print(f"My School Name is {school_name}")
+print(f"My Marks is {marks}")
+print(f"Value of pi is {pi}")
+print(f"Am I a Student? {is_Student}")
+print(type(Name))
+print(type(Age))
+print(type(pi))
+print(type(is_Student))
+my_number = 10
+print(f"My Number is: {my_number}")
+print(type(my_number))
+my_text = "Hello i am keerthu"
+print(f"My Text is: {my_text}")
+print(type(my_text))
